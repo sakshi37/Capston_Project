3511217682a1164703f8348931c7c26d89013881
Bhushan Alhat <alhatbhushan@gmail.com>
2025-04-03 14:18:27 +0530

    remove user id from loan request

--- LibraryManagement.Api/Controllers/LoanController.cs
+++ LibraryManagement.Api/Controllers/LoanController.cs
@@ -3,41 +3,42 @@ using AutoMapper;
 using LibraryManagement.Api.DTO;
 using LibraryManagement.Application.IService;
 using LibraryManagement.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagement.Api.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class LoanController : ControllerBase
     {
         readonly ILoanService _loanService;
         readonly IMapper _mapper;
+        
         public LoanController(ILoanService iLoanService, IMapper mapper)
-
         {
             _mapper = mapper;
             _loanService = iLoanService;
         }
+        
         [HttpPost]
         public async Task<ActionResult<LoanResponseDto>> AddLoan([FromBody] LoanRequestDto loanRequest)
         {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
             var loan = _mapper.Map<Loan>(loanRequest);
+            loan.UserId = userId;
+
             var addedLoan = await _loanService.AddLoan(loan);
             return Ok(addedLoan);
         }
+        
         [HttpGet]
         public async Task<IActionResult> GetLoansByUser()
         {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
             var loan = await _loanService.GetLoansByUser(userId);
             return Ok(loan);
         }
-
-
-
-
     }
-
 }
--- LibraryManagement.Api/DTO/LoanRequestDto.cs
+++ LibraryManagement.Api/DTO/LoanRequestDto.cs
@@ -2,8 +2,6 @@
 {
     public class LoanRequestDto
     {
-        //this is temp this will be remove once jwt implement
-        public string UserId { get; set; }
         public int BookId { get; set; }
 
         public DateTime EndDate { get; set; }
